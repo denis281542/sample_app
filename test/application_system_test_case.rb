2dcb4325f5e0363e42d2563e973a3bf91642e2d2
denis281542 <deniskuimov@mail.ru>
2019-12-19 17:31:20 +0500

    Finish layout and routes

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,8 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  test "full title helper" do
+    assert_equal full_title, "Sing up"
+    assert_equal full_title("Help"), «Sing up | Help»
+  end
 end
