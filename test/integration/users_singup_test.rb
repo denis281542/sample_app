17b6f840d0c09f722f4af4fd00496afab8a85c8e
denis281542 <deniskuimov@mail.ru>
2019-12-22 19:02:39 +0500

    Added tests valid signup information and invalid signup information in users_singup_test.rb

--- test/integration/users_singup_test.rb
+++ test/integration/users_singup_test.rb
@@ -4,11 +4,24 @@ class UsersSingupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get singup_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name: " ", 
+      post users_path, params: { user: { name: " ", 
                                email: "user@invalid", 
-                               passord: "foo", 
-                               password_confirmation: "bar" }  
+                               passord:               "foo", 
+                               password_confirmation: "bar" } } 
     end
     assert_template 'users/new'
   end
+
+  test "valid signup information" do
+  get singup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                          email: "user@example.com",
+                                          password:              "password",
+                                          password_confirmation: "password" } } 
+      follow_redirect!
+    end
+    assert_template 'users/show'
+  end
+
 end
