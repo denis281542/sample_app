eced6b0e18cee646fd1232de8f1eca0ef2b1d1bb
denis281542 <deniskuimov@mail.ru>
2019-12-23 23:32:59 +0500

    Added view login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/new'
-  root 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'singup' => 'users#new'
-  get 'login' => 'sessions#new'
+  root               'static_pages#home'
+  get 'help'      => 'static_pages#help'
+  get 'about'     => 'static_pages#about'
+  get 'contact'   => 'static_pages#contact'
+  get 'singup'    => 'users#new'
+  get 'login'     => 'sessions#new'
+  post 'login'    => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
