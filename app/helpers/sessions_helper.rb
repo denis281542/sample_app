ec4d7eb8812ee587ba62c50861cffe6318ddab7b
denis281542 <deniskuimov@mail.ru>
2019-12-29 00:42:10 +0500

    Added forget in model User

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,8 +8,8 @@ module SessionsHelper
   #Запоминает пользователя в постоянном сеансе
   def remember(user)
     user.remember
-    cookie.permanent.signed(:user_id) = user.id
-    cookie.permanent[:remember_token] = user.remember_token
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   # Возвращает текущего пользователя, соответсыующего токену в cookie.
@@ -17,8 +17,8 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
     elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id :user_id)
-      if user && user.authentacated?(cookies[:remember_token])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
