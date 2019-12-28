84e10ec8175e24e33ed71884b74a23b14a1fa7e4
denis281542 <deniskuimov@mail.ru>
2019-12-28 23:12:24 +0500

    Added cookie in current_user sessions_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,9 +12,17 @@ module SessionsHelper
     cookie.permanent[:remember_token] = user.remember_token
   end
 
-  # Возвращает текущего вошедшего пользователя (если есть).
+  # Возвращает текущего пользователя, соответсыующего токену в cookie.
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id :user_id)
+      if user && user.authentacated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # Возвращает true, если пользователь зарегистрирован, иначе возвращает false.
