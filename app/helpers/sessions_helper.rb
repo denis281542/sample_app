6955becba36585831deba73cf322edda9726958a
denis281542 <deniskuimov@mail.ru>
2019-12-28 22:31:45 +0500

    Added authenticated?(remember_token) in sessinons_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id   
   end
 
+  #Запоминает пользователя в постоянном сеансе
+  def remember(user)
+    user.remember
+    cookie.permanent.signed(:user_id) = user.id
+    cookie.permanent[:remember_token] = user.remember_token
+  end
+
   # Возвращает текущего вошедшего пользователя (если есть).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
