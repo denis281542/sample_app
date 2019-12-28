cb3b8ed7a2857388e5cb04d545dc3152cd5af253
denis281542 <deniskuimov@mail.ru>
2019-12-29 00:46:33 +0500

    Added forget(user) in model sessions_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,6 +30,12 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Осуществляет выход текущего пользователя.
   def log_out
     session.delete(:user_id)
