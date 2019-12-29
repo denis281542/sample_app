8fb09a187eac09cdcbb0d49684ddc039b29c6c08
denis281542 <deniskuimov@mail.ru>
2019-12-30 00:06:58 +0500

    Edit model user @ for user_singup_test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in user
-      remember user
+      log_in @user
+      remember @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to user
+      redirect_to @user
     else
       render 'new'
     end
