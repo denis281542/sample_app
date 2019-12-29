706a113f7e6323f2c812c7af075b0b880cf0bd1e
denis281542 <deniskuimov@mail.ru>
2019-12-30 00:20:45 +0500

    Added return false if remember_digest.nil?

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,7 @@ class User < ApplicationRecord
 
   # Возвращает true, если указанный токен соответствует дайджесту.
   def authenticated?(remember_token)
-    return false if remember_digest.nil?
+      return false if remember_digest.nil?
       BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
   
