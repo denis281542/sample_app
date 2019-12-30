e6ff8e805a2b6db1ce79c0999a05203bcf095a85
denis281542 <deniskuimov@mail.ru>
2019-12-30 09:13:41 +0500

    Added log_in_as test/test_helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,4 +13,20 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  def log_in_as
+    if integrations_test?
+      post login_path, session: { email: user.email
+                                  password: password
+                                  remember_me: remember_me }
+    else 
+      session[:user_id] = user_id
+  end
 end
+
+private
+  
+  #Возвращает true внутри интергационного теста.
+  def integtation_test?
+    defined?(post_via_redirect)
+  end
