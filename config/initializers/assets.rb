695a2381e4f3a38bf019db430fddbede75b1b2df
denis281542 <deniskuimov@mail.ru>
2020-01-02 20:28:33 +0500

    Added method edit in users_controller

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,9 +6,10 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.precompile += %w( rails-ujs.js )
+
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.precompile += %w( rails-ujs.js )
 
 
 # Precompile additional assets.
