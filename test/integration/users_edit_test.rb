dfccf732208228ce0abf5989dfbebf072be18aba
denis281542 <deniskuimov@mail.ru>
2020-01-04 01:09:59 +0500

    Added test 'seccessful edit'

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -13,4 +13,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                     password_confirmation: "word" } }
     assert_template "users/edit"                                
   end
+
+  test "seccessful edit" do
+    get edit_user_path(@user)
+    assert_template "users/edit"
+    name = Den
+    email = den@mail.ru
+    patch user_path(@user), params: { user: { name: name,
+                                               email: email,
+                                               password: "",
+                                               password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+  end
 end
