3330f7fa3d230c8c3e7e007686571ccbba9d82fd
denis281542 <deniskuimov@mail.ru>
2020-01-05 19:53:37 +0500

    Added test 'should redirect update when logged in as wrong user'

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -25,8 +25,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect edit when logged in as wrong user" do
-    log_on_as(@other_user)
+    log_in_as(@other_user)
     get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch '/users/update', params: { id: @user.id, user: { name: @user.name, email: @user.email } }
     assert flash.empty?
     assert_redirected_to root_url
   end
