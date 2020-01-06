b799846c592b1c6a084fd580522467624286eb34
denis281542 <deniskuimov@mail.ru>
2020-01-06 14:53:24 +0500

    Added 'should redirect index when not logged in'

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:grigory)
   end
 
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get singup_path
     assert_response :success
