db638c1feb7adeca3d562502e8ae5274adc1e9ef
denis281542 <deniskuimov@mail.ru>
2020-01-12 20:42:46 +0500

    Edit user.yml

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -9,8 +9,8 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   test "index including pagination" do
     log_in_as(@user)
     get users_path
-    assert_template 'users/index'
     puts @response.body
+    assert_template 'users/index'
     assert_select 'div.pagination'
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
