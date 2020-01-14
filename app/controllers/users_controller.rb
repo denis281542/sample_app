451cc44bc76ebdfa69bbeaeb4e03b7a3a7af90cf
denis281542 <deniskuimov@mail.ru>
2020-01-14 11:00:46 +0500

    Added before_action admin_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,6 +71,6 @@ class UsersController < ApplicationController
 
   # Подтверждает наличие прав администратора
   def admin_user
-    redirect_to(root_url) unless current_user.admin?
+    redirect_to(root_url)unless current_user.admin?
   end
 end
