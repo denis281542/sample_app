a0f3c8b3971d44b914eebd21553ad6b821f5bacd
denis281542 <deniskuimov@mail.ru>
2020-01-14 17:08:24 +0500

    Edit test 'index including pagination and delete links'

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,7 +7,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:grigory)
   end
 
-  test "index including pagination" do
+  test "index including pagination and delete links" do
     log_in_as(@user)
     get users_path
     assert_template 'users/index'
@@ -18,8 +18,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
       unless user == @admin
         assert_select 'a[href=?]', users_path(user), text: 'delete'
                                                      method: :delete
+      end
+    end
+    assert_difference 'User.count' do
+      delete user_path(@non_admin)
     end
   end
-
 
 end
