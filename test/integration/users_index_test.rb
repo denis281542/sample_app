7c4b3a689753ac9ccfe32ddb8747e8bf55668c61
denis281542 <deniskuimov@mail.ru>
2020-01-14 17:04:55 +0500

    Added def setup user non-admin

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
   
   def setup
-    @user = users(:denis)
+    @admin     = users(:denis)
+    @non_admin = users(:grigory)
   end
 
   test "index including pagination" do
@@ -11,8 +12,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    User.paginate(page: 1).each do |user|
+    first_page_of_user = User.paginate(page: 1)
+    first_page_of_user.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', users_path(user), text: 'delete'
+                                                     method: :delete
     end
   end
+
+
 end
