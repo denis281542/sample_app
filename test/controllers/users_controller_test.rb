15b63b1fb767a56ef97726688fa4d691865a4466
denis281542 <deniskuimov@mail.ru>
2020-01-15 20:42:58 +0500

    Edit test 'should redirect destroy when not logged in'

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -54,7 +54,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
     assert_no_difference 'User.count' do
-      delete user_url(@other_user)
+      delete :destroy, params: { id: @user }
     end
     assert_redirected_to root_url
   end
